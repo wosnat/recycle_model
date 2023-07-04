f48afaa1b061b666dc345fe4e5f523362f86b15a
Osnat Weissberg <osnat.weissberg@gmail.com>
2023-07-04 11:00:31 +0300

    pro99 baseline to separate files

--- run_files/run_baseline_ponly_pro99.bash
+++ run_files/run_baseline_ponly_pro99.bash
@@ -4,7 +4,7 @@ mkdir -p ~/work/RECYCLE_MODEL/results/baseline/log
 for m in MIN LEAK MIXO FULL
 do
 	       echo $m 
-~/work/RECYCLE_MODEL/recycle_model/model_equations_separate_NC_sep_vmax.py --ref_csv ~/work/RECYCLE_MODEL/recycle_model/refence_pro99_axenic.xlsx  --pro99_mode --outdpath ~/work/RECYCLE_MODEL/results/baseline/out/ --run_id baseline_ponly_${m} --model ${m}  --which_organism ponly --json ~/work/RECYCLE_MODEL/recycle_model/run_files/empty.json > ~/work/RECYCLE_MODEL/results/baseline/log/baseline_ponly_${m}.log 2>&1 &
+~/work/RECYCLE_MODEL/recycle_model/model_equations_separate_NC_sep_vmax.py --ref_csv ~/work/RECYCLE_MODEL/recycle_model/refence_pro99_axenic.xlsx  --pro99_mode --outdpath ~/work/RECYCLE_MODEL/results/baseline/out/ --run_id baseline_ponly_pro99_${m} --model ${m}  --which_organism ponly --json ~/work/RECYCLE_MODEL/recycle_model/run_files/empty.json > ~/work/RECYCLE_MODEL/results/baseline/log/baseline_ponly_${m}.log 2>&1 &
 
 
    done
