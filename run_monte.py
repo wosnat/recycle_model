03ea49c12dd0aab0047a738d71c881aadb33509d
wosnat <osnat.weissberg@gmail.com>
2023-07-06 14:47:35 +0300

    Update run_monte.py

--- run_monte.py
+++ run_monte.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,29 +9,48 @@ import pprint
 sns.set(style="white", context='poster')
 from sympy import *
 import math
+init_printing(use_unicode=True)
 from scipy.integrate import solve_ivp
-from sklearn.metrics import mean_squared_error
-import json
-import subprocess
-import sys
-import re
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pprint
-from sympy import *
-import math
-from scipy.integrate import solve_ivp
-from scipy.optimize import differential_evolution
-from itertools import combinations
-
-
-from SALib.sample import saltelli
-from SALib.analyze import sobol
+from scipy.optimize import least_squares
+from functools import lru_cache
 
 from model_equations_separate_NC_sep_vmax import *
+
+
+def run_model(random_params, random_values):
+    lowN_run = generate_json_and_run_from_X(
+        random_values, random_params, param_vals, 
+        ref_fpath, json_dpath, out_dpath, run_id, 
+            timeout=timeout, log_params=log_params,
+        which_organism=which_organism, pro99_mode=False,
+    )
+
+# this wrap is to cache run_model results
+run_model = lru_cache(run_model)
+
+def wrap_run_model(X):
+    return(run_model(tuple(X)))
+
+
+
+def create_random_param_vals(i):
+    random_number_of_parameters = i[0]
+    random_parameter_values = i[1:]
+    random_params_to_update = rng.choice(
+        len(params_to_update), replace=False, size=random_number_of_parameters)
+    random_params = [params_to_update[j] for j in random_params_to_update ]
+    random_values = [random_parameter_values[j] for j in random_params_to_update ]
+    return random_params, random_values
+
+    
+def run_monte_carlo(number_of_runs):
+    rng = np.random.default_rng()
+    number_of_params = rng.integers(low=2,high=6, size=number_of_runs)
+    random_param_values = [rng.uniform(low=l, high=h, size=number_of_runs) for l,h in bounds_logged]
+    for i in zip(number_of_params, *random_param_values):
+        random_params, random_values = create_random_param_vals(i)
+        print (random_params, random_values)
+            
 
 
 if __name__ == '__main__':
@@ -40,60 +58,46 @@ if __name__ == '__main__':
     import json
     import pprint
 
-    # def generate_json_and_run_from_X(X,  json_dpath, out_dpath, out_fprefix, timeout=10*60):
     parser = argparse.ArgumentParser(description='Run models - nutrients recycle with separate N/C and quotas.')
-    parser.add_argument('--ref_csv', help='reference CSV', default='prelim bottle.csv')
+    parser.add_argument('--ref_csv', help='reference CSV', required=True)
+    parser.add_argument('--vpro_json', help='json file used for pro params', required=True)
     parser.add_argument('--json_dpath', help='folder to put json files with param vals', default='.')
-
     parser.add_argument("--out_dpath", help="output dir", default='.')
     parser.add_argument("--run_id", help="run id", required=True)
     parser.add_argument("--timeout", help="timeout",  type=int, default=2*60)
-    parser.add_argument("--number_of_runs", help="number of simulations to run",  type=int, default=1000)
-    parser.add_argument("--model", help="model to run", choices=['MIN', 'FULL', 'LEAK', 'MIXO'], default='FULL')
-    parser.add_argument("--organism_to_tune", help="which organism to tune", choices=['PRO', 'HET'], default='PRO')
-    parser.add_argument("--which_organism", help="which organism to run", choices=['ponly', 'honly', 'all'], default='all')
-    parser.add_argument('--ref_sample_id', help='reference sample id for fitting', required=True, type=int)
-    parser.add_argument('--vpro_json', help='json file used for pro params', required=True)
-                        
+    parser.add_argument("--model", help="model to run", choices=['MIN', 'FULL', 'LEAK', 'MIXO'], default='MIN')
+    parser.add_argument("--number_of_runs", help="number of simulations to run",  type=int, default=1024)
+          
     
     args = parser.parse_args()
     dpath = args.out_dpath
     if dpath != '':
         os.makedirs(dpath, exist_ok=True)
     os.makedirs(args.json_dpath, exist_ok=True)
-    
-    #refdf = pd.read_excel(args.ref_csv)
-    param_vals = get_param_vals(args.model)
 
-    params_to_update, bounds, log_params = get_param_tuning_values(args.model, args.organism_to_tune)
-    run_id = f"{args.run_id}_{args.model}"
+    model = args.model
+    organism_to_tune = 'HET'
+    which_organism='all'
+    run_id = args.run_id
 
+    json_dpath = args.json_dpath
+    out_dpath = args.out_dpath
+    timeout= args.timeout
+    vpro_json = args.vpro_json
 
-    if args.gen_sensitivity:
-        problem = {
-            'num_vars': len(params_to_update),
-            'names': params_to_update,
-            'bounds': bounds,
-        }
-        param_values = saltelli.sample(problem, 1024)
-        print(param_values.shape)
-        np.savetxt(args.params_txt, param_values)
-
-        
-    elif args.param_sensitivity != -1:
-        idx = args.param_sensitivity
-        run_sensitivity_per_parameter(param_vals, params_to_update[idx], bounds[idx], args.number_of_runs, 
-            run_id, args.ref_csv, args.json_dpath, args.out_dpath, args.timeout, log_param=log_params[idx], 
-            which_organism=args.which_organism, pro99_mode=args.pro99_mode,
-        )
-        
-    else:
-        param_values = np.loadtxt(args.params_txt)
-        run_chunk(param_vals, 
-            param_values, params_to_update, args.chunk, args.number_of_runs, 
-            run_id, args.ref_csv, args.json_dpath, args.out_dpath, 
-            args.timeout, log_params=log_params, which_organism=args.which_organism, pro99_mode=args.pro99_mode
-        )
+    ref_fpath =  args.ref_csv
+    number_of_runs = args.number_of_runs
 
 
+    param_vals = get_param_vals(model)
+    param_vals = json2params(param_vals, vpro_json)
 
+    params_to_update, bounds, log_params = get_param_tuning_values(model, organism_to_tune)
+
+    # start with the defalt params
+    # x0 = np.array([np.log(param_vals[i]) if lg else param_vals[i] for i, lg in zip(params_to_update, log_params)])
+
+    bounds_logged = [(np.log(b[0]),  np.log(b[1]))  if lg else b for b,lg in zip(bounds, log_params)]
+    param_bounds =  list(zip(*bounds_logged))
+
+    run_monte_carlo(number_of_runs)
