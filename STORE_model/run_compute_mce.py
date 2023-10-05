6b14684a0f1a24821d870d2ab580386311e72ac8
Osnat Weissberg <osnat.weissberg@gmail.com>
2023-10-05 13:25:31 +0300

    fix typos

--- STORE_model/run_compute_mce.py
+++ STORE_model/run_compute_mce.py
@@ -1,12 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pprint
-sns.set(style="white", context='poster')
 import math
-from scipy.integrate import solve_ivp
-
+from scipy.stats import gmean
+import os
 from model_equations_separate_NC_store_numba import *
 
 
@@ -63,17 +62,16 @@ def all_loss_functions(f, f_scale):
     })
     
 
-
 def _mse_loss(x, refdf, refcol, col, timecol, tolerance, loss, f_scale):
     #print(x.columns)
     tdf = pd.merge_asof(x, refdf[[timecol] + refcol], on=timecol, direction='nearest', tolerance=tolerance).dropna()
-    
+    colname = f'{loss}_{f_scale}'
     #mse_series = pd.concat([all_loss_functions(f'RMSE_{c}', tdf[c] - tdf[rc], f_scale) for c,rc in zip(col, refcol)])
-    mse_series_list = [loss_function(tdf[c] - tdf[rc], loss, f_scale) for c,rc in zip(col, refcol)]
-    mse_series = np.sqrt(mse_series_list[0] * mse_series_list[1])
+    mse_dict = {f'{colname}_{c}': loss_function(tdf[c] - tdf[rc], loss, f_scale) for c,rc in zip(col, refcol)}
+    mse_dict[colname] = gmean([mse_dict[f'{colname}_{c}'] for c in col])
+    mse_dict['compare_points'] = tdf.shape[0]
+    return pd.Series(mse_dict)
     
-    mse_series['compare_points'] = tdf.shape[0]
-    return mse_series
 
 def compute_mse_loss(
     df, refdf, 
@@ -97,44 +95,46 @@ if __name__ == '__main__':
     import pprint
 
     parser = argparse.ArgumentParser(description='post process results')
-    parser.add_argument('--dpath', #action='append', 
-            help='paths to load from', required=True, nargs="+")
+    parser.add_argument('--incsv', help='file with results', required=True)
     parser.add_argument("--outdpath", help="output dir", default='.')
     parser.add_argument("--run_id", help="run id", required=True)
     parser.add_argument('--ref_csv', help='reference CSV', default='None')
     parser.add_argument('--loss', help='loss function', choices=['linear', 'huber', 'softl1', 'cauchy', 'arctan'], required=True)
     parser.add_argument("--f_scale", help="f_scale",  type=float, required=True)
-
+    parser.add_argument("--which_organism", help="which organism to run", choices=['ponly', 'honly', 'all'], default='all')
 
     args = parser.parse_args()
-    dpath = args.outdpath
-    if dpath != '':
-        os.makedirs(dpath, exist_ok=True)
+    outdpath = args.outdpath
+    if outdpath != '':
+        os.makedirs(outdpath, exist_ok=True)
         
     if args.ref_csv == 'None':
         refdf = None
     else:
         refdf = pd.read_excel(args.ref_csv)
+        refdf['ref_Bp[N]'] = refdf['ref_Bp[N]'].clip(lower=4)
+        refdf['ref_Bp[C]'] = refdf['ref_Bp[C]'].clip(lower=4)
 
 
-    dpath = 'results'
 
-
+    loss = args.loss
+    f_scale=args.f_scale
     pro99_mode = False 
-    which_organism = 'all'
-    organism_to_tune = 'HET'
+    which_organism = args.which_organism
     (var_names, init_var_vals, intermediate_names, calc_dydt, prepare_params_tuple
     ) = get_constants_per_organism(pro99_mode, which_organism)
 
 
-    df = pd.read_csv(os.path.join(dpath,'monte_df.csv.gz',))
+    df = pd.read_csv(args.incsv)
 
 
-    min_values = df[var_names+ ['idx']].groupby('idx').min().min(axis=1)
+    min_values = df[var_names+ ['run_id']].groupby('run_id').min().min(axis=1)
     bad_ids_negative_values = set(min_values[min_values< -1e-9].index)
     bad_ids =bad_ids_negative_values
-    df     =     df.loc[    ~df.idx.isin(bad_ids)].copy()
+    df     =     df.loc[    ~df.run_id.isin(bad_ids)].copy()
 
-    df.groupby(['run_id', 'model']).apply(
-            lambda x: compute_mse_loss(x, refdf=refdf, f_scale=f_scale)).reset_index()
+    msedf = df.groupby(['run_id']).apply(
+            lambda x: compute_mse_loss(x, refdf=refdf, f_scale=f_scale, loss=loss)).reset_index()
+
+    msedf.to_csv(os.path.join(outdpath, f'{args.run_id}_loss_{loss}_{f_scale}.csv.gz'), compression='gzip', index=False)
 
